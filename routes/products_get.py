e0af47f96a1ebf2b47f695d27192ce69db7272b1
Maurizio Prizzi <maurizioprizzi@gmail.com>
2023-05-29 22:42:11 -0300

    products get from id

--- routes/products_get.py
+++ routes/products_get.py
@@ -27,3 +27,31 @@ def get_all_products():
 
     # Retorna os produtos paginados
     return jsonify(paginated_products)
+
+# Rota para mostrar um produto específico
+@products_get_routes.route('/products/<int:product_id>', methods=['GET'])
+def get_product(product_id):
+    conn = sqlite3.connect('estoque.db')
+    cursor = conn.cursor()
+
+    # Obtém o produto do banco de dados pelo ID
+    cursor.execute('SELECT * FROM products WHERE id = ?', (product_id,))
+    product = cursor.fetchone()
+
+    conn.close()
+
+    # Verifica se o produto existe
+    if not product:
+        return jsonify({'error': 'Produto não encontrado'}), 404
+
+    # Cria um dicionário com os dados do produto
+    product_dict = {
+        'id': product[0],
+        'name': product[1],
+        'quantity': product[2],
+        'category_id': product[3],
+        'category_name': product[4]
+    }
+
+    # Retorna o produto encontrado
+    return jsonify(product_dict), 200
