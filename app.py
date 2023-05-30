fb5c5d1f77cd091365b1d4295123102a08c55aab
Maurizio Prizzi <maurizioprizzi@gmail.com>
2023-05-29 21:08:12 -0300

    products post

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from routes.auth_refresh import auth_refresh_routes
 from routes.categories_get import categories_get_routes
 # Importa a rota de inserção de categorias do arquivo routes/categories_post.py
 from routes.categories_post import categories_post_routes
+# Importa a rota de inserção de produtos do arquivo routes/products_post.py
+from routes.products_post import products_post_routes
 
 # Registra as rotas de autenticação no aplicativo Flask
 app.register_blueprint(auth_routes)
@@ -19,6 +21,8 @@ app.register_blueprint(auth_refresh_routes)
 app.register_blueprint(categories_get_routes)
 # Registra a rota de inserção de categorias no aplicativo Flask
 app.register_blueprint(categories_post_routes)
+# Registra a rota de inserção de produtos no aplicativo Flask
+app.register_blueprint(products_post_routes)
 
 # Configuração do JWT
 from flask_jwt_extended import JWTManager
--- criar_db.py
+++ criar_db.py
@@ -3,11 +3,15 @@ import sqlite3
 conn = sqlite3.connect('estoque.db')
 cursor = conn.cursor()
 
-# Cria a tabela "categories" no banco de dados
+# Cria a tabela "products" no banco de dados
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS categories (
+    CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY,
-        name TEXT
+        name TEXT,
+        quantity INTEGER,
+        category_id INTEGER,
+        category_name TEXT,
+        FOREIGN KEY (category_id) REFERENCES categories (id)
     )
 ''')
 
