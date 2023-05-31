1df7fadfe01185fda2285e84197eb4fd99d8234e
Maurizio Prizzi <maurizioprizzi@gmail.com>
2023-05-31 00:21:43 -0300

    ajustes

--- tests/test_products_get.py
+++ tests/test_products_get.py
@@ -1,5 +1,3 @@
-# test_products_get.py
-
 import unittest
 from unittest.mock import patch, Mock
 from flask import Flask
@@ -12,36 +10,38 @@ class ProductsGetTestCase(unittest.TestCase):
         self.app.register_blueprint(products_get_routes)
         self.client = self.app.test_client()
 
+    # Teste para obter todos os produtos com sucesso
     @patch('sqlite3.connect')
     def test_get_all_products(self, mock_connect):
-        # Create a mock cursor that fetches some dummy products
+        # Cria um cursor fictício que retorna alguns produtos fictícios
         mock_cursor = Mock()
         mock_cursor.fetchall.return_value = [
-            (1, 'Product 1', 10, 1, 'Categoria 1'), 
-            (2, 'Product 2', 20, 2, 'Categoria 2')
+            (1, 'Produto 1', 10, 1, 'Categoria 1'), 
+            (2, 'Produto 2', 20, 2, 'Categoria 2')
         ]
 
-        # Make connect() return a mock connection that returns our mock cursor
+        # Faz connect() retornar uma conexão fictícia que retorna o cursor fictício
         mock_conn = Mock()
         mock_conn.cursor.return_value = mock_cursor
         mock_connect.return_value = mock_conn
 
         response = self.client.get('/products')
-        data = response.get_json()
+        data = response.get_json()['data']  # Acessa a lista de produtos dentro da chave 'data'
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data, [
-            {'id': 1, 'name': 'Product 1', 'quantity': 120, 'category_id': 1, 'category_name': 'Categoria 1'}, 
-            {'id': 1, 'name': 'Product 2', 'quantity': 60, 'category_id': 2, 'category_name': 'Categoria 2'}
+            {'id': 1, 'name': 'Produto 1', 'quantity': 10, 'category_id': 1, 'category_name': 'Categoria 1'},
+            {'id': 2, 'name': 'Produto 2', 'quantity': 20, 'category_id': 2, 'category_name': 'Categoria 2'}
         ])
 
+    # Teste para obter um produto específico com sucesso
     @patch('sqlite3.connect')
     def test_get_product(self, mock_connect):
-        # Create a mock cursor that fetches a dummy product
+        # Cria um cursor fictício que retorna um produto fictício
         mock_cursor = Mock()
-        mock_cursor.fetchone.return_value = (1, 'Product 1', 10, 1, 'Categoria 1')
+        mock_cursor.fetchone.return_value = (1, 'Produto 1', 10, 1, 'Categoria 1')
 
-        # Make connect() return a mock connection that returns our mock cursor
+        # Faz connect() retornar uma conexão fictícia que retorna o cursor fictício
         mock_conn = Mock()
         mock_conn.cursor.return_value = mock_cursor
         mock_connect.return_value = mock_conn
@@ -50,15 +50,16 @@ class ProductsGetTestCase(unittest.TestCase):
         data = response.get_json()
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(data, {'id': 1, 'name': 'Product 1', 'quantity': 120, 'category_id': 1, 'category_name': 'Categoria 1'})
+        self.assertEqual(data, {'id': 1, 'name': 'Produto 1', 'quantity': 10, 'category_id': 1, 'category_name': 'Categoria 1'})
 
+    # Teste para obter um produto que não existe
     @patch('sqlite3.connect')
     def test_get_product_not_found(self, mock_connect):
-        # Create a mock cursor that fetches no product
+        # Cria um cursor fictício que não retorna nenhum produto
         mock_cursor = Mock()
         mock_cursor.fetchone.return_value = None
 
-        # Make connect() return a mock connection that returns our mock cursor
+        # Faz connect() retornar uma conexão fictícia que retorna o cursor fictício
         mock_conn = Mock()
         mock_conn.cursor.return_value = mock_cursor
         mock_connect.return_value = mock_conn
